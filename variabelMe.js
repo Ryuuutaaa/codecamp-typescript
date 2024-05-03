f5921562d0ca0c6f10aa5340caebbc36799b15b0
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-05-03 09:15:16 +0800

    number and boolean

--- variabelMe.js
+++ variabelMe.js
@@ -1,2 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var greetings = "Helllo Wolrd";
+greetings.toLowerCase();
 console.log(greetings);
+// number
+var userId = 123344;
+var idMahasiswa;
+idMahasiswa = 1122121121;
+// boolean
+var isLogin = true;
--- variabelMe.ts
+++ variabelMe.ts
@@ -1,5 +1,15 @@
 let greetings: string = "Helllo Wolrd";
 
+greetings.toLowerCase();
 console.log(greetings);
+
+// number
+let userId: number = 123344;
+
+let idMahasiswa: number;
+idMahasiswa = 1122121121;
+
+// boolean
+let isLogin: boolean = true;
 
 export {};
