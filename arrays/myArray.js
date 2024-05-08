9b88b6a990b5e44047d865a8a05a4f2aa055c15c
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-05-08 11:10:32 +0800

    array

---
+++ arrays/myArray.js
@@ -0,0 +1,7 @@
+var superHero = [];
+var heroPower = [];
+var allUser = [];
+var MLModels = [[255, 255, 255], []];
+superHero.push("Rafi akmal");
+heroPower.push(10);
+allUser.push({ name: "", isActive: true });
---
+++ arrays/myArray.ts
@@ -0,0 +1,17 @@
+const superHero: string[] = [];
+
+const heroPower: Array<number> = [];
+
+type User = {
+  name: string;
+  isActive: boolean;
+};
+
+const allUser: User[] = [];
+
+const MLModels: number[][] = [[255, 255, 255], []];
+
+superHero.push("Rafi akmal");
+heroPower.push(10);
+
+allUser.push({ name: "", isActive: true });
