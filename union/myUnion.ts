cf2a8fe0c9199f288ac3854eaa760448366ad1bf
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-05-10 20:06:59 +0800

    to array

--- union/myUnion.ts
+++ union/myUnion.ts
@@ -1,3 +1,25 @@
 let score: number | string = 33;
- score = 35
- score = "3434"
+score = 35;
+score = "3434";
+
+type User = {
+  name: string;
+  id: number;
+};
+
+type Admin = {
+  name: string;
+  id: number;
+};
+
+let hitesh: User | Admin = { name: "hitest", id: 23 };
+
+hitesh = { name: "rafi", id: 2 };
+
+function geDbId(id: number | string) {
+  if (typeof id === "string") {
+    id.toLowerCase();
+  }
+}
+
+
