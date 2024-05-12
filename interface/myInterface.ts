f892846f75f6051b6c9917905a9c1e0b64206b7d
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-05-13 01:21:53 +0800

    interface vs type who is better??

--- interface/myInterface.ts
+++ interface/myInterface.ts
@@ -7,10 +7,21 @@ interface User {
   getCouppon(coupponName: string, value: number): number;
 }
 
-const hitest: User = {
+interface User {
+  githubToke: number;
+}
+
+interface Admin extends User {
+  role: "admin" | "student" | "teacher";
+}
+
+// perbedaan menggunakan interface dan type adalah interface bisa menggunakan extends kepada parentnya sedangkan type tidak bisa menggunakan extend
+
+const hitest: Admin = {
   dbId: 33,
   email: "rea@gmail",
   userId: 23,
+  role: "admin",
   googleId: 23,
   startTrail: () => {
     return "haii";
@@ -18,4 +29,5 @@ const hitest: User = {
   getCouppon(name: "larisacipiont", off: 10) {
     return 34;
   },
+  githubToke: 34,
 };
