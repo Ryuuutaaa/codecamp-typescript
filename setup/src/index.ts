28471b7beb83b7638cb87e9c5fda29f96716aff0
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-05-21 11:32:02 +0800

    private public

--- setup/src/index.ts
+++ setup/src/index.ts
@@ -1,13 +1,21 @@
-class User {
-  email: string;
-  name: string;
-  readonly city: string = "bali";
+// class User {
+//   public email: string;
+//   private name: string;
+//   private readonly city: string = "bali";
 
-  constructor(email: string, name: string) {
-    this.email = email;
-    this.name = name;
-  }
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+
+class User {
+  constructor(
+    public email: string,
+    public name: string
+  ) // private userId: string
+  {}
 }
 
 const rafi = new User("ryu@tgmail.com", "ruy");
-// rafi.city = "bali";
+// rafi.city;
